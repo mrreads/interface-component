1d0da62121c4f10831dd00ad303b278181db111d
Alexander <mrreads@yandex.com>
2020-11-08 14:30:40 +0300

    save logic

--- js/save.js
+++ js/save.js
@@ -1,13 +1,38 @@
 function save()
 {
     let componentToSave = document.querySelectorAll("#wrapper .component");
+    let jsonWithComponent = [];
 
     componentToSave.forEach(component => {
         
         if (component.classList.contains('heading'))
         {
-
+            let jsonToAdd = 
+            {
+                "name": "heading",
+                "text": component.querySelector('.heading-text').textContent,
+                "post-text": component.querySelector('.heading-post-text').textContent,
+                "image-src": component.querySelector('img').src
+            };
+            jsonWithComponent.push(jsonToAdd);
         }
-        
     });
+
+
+    let gameData = new FormData();   
+    gameData.append('json', JSON.stringify(jsonWithComponent));
+
+    fetch('./php/updateJSON.php', 
+    {
+        method: 'POST',
+        body: gameData
+    })
+    .then(result =>
+    {
+        result.json().then(data => 
+        {
+            console.log(data);     
+        });
+    });
+
 }
