9c477dafeaa0407c2616dee7f02ff40f96aa4ab9
Alexander <mrreads@yandex.com>
2020-11-08 14:05:44 +0300

    add save button

---
+++ js/save.js
@@ -0,0 +1,13 @@
+function save()
+{
+    let componentToSave = document.querySelectorAll("#wrapper .component");
+
+    componentToSave.forEach(component => {
+        
+        if (component.classList.contains('heading'))
+        {
+
+        }
+        
+    });
+}
